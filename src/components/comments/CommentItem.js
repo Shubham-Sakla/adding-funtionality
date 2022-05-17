1d462022953755d82609db604c0d9d4088103ea1
Shubham-Sakla <shubhamsa@mitrmedia.com>
2022-05-17 17:56:45 +0530

    add a comment

--- src/components/comments/CommentItem.js
+++ src/components/comments/CommentItem.js
@@ -1,11 +1,12 @@
-import classes from './CommentItem.module.css';
+import classes from './CommentItem.module.css'
 
 const CommentItem = (props) => {
+  console.log('Welome to Commmit')
   return (
     <li className={classes.item}>
       <p>{props.text}</p>
     </li>
-  );
-};
+  )
+}
 
-export default CommentItem;
+export default CommentItem
